f24df1764c941b81debf97e0c1fc341b1bed38ac
Shubham Gupta <shubhamg2@hexaware.com>
2018-04-02 12:21:45 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var zendesk = require('node-zendesk');
+var exampleConfig = require('./config');
+//var fs = require('fs');
+//var zd = require('../lib/client');
+
+var client = zendesk.createClient({
+  username:  exampleConfig.auth.username,
+  token:     exampleConfig.auth.token,
+  remoteUri: exampleConfig.auth.remoteUri
+});
+
+client.users.auth(function (err, res, result) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(JSON.stringify(result));
+  console.log(JSON.stringify(result.verified, null, 2, true));
+});
---
+++ config.js
@@ -0,0 +1,5 @@
+exports.auth = {
+  username: 'humanbot@zendesk.com',
+  token: 'CIRIKdRSPw51G9ugyrAzzKDTwJbc7iLdFrnix1ay',
+  remoteUri: 'https://humanbot.zendesk.com/api/v2'
+};
