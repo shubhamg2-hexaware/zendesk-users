219896c6be8ccb86ca544da79315ac5950f174a1
Shubham Gupta <shubhamg2@hexaware.com>
2018-04-02 15:13:42 +0530

    username changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.auth = {
-  username: 'wrestlingmania9@gmail.com',
+  username: 'shubham.gupta.ec13@ggits.net',
   token: 'CIRIKdRSPw51G9ugyrAzzKDTwJbc7iLdFrnix1ay',
   remoteUri: 'https://humanbot.zendesk.com/api/v2'
 };
