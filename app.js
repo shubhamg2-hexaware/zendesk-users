e55d74b36a47f5a550691beac4f17aaf7c998de8
Shubham Gupta <shubhamg2@hexaware.com>
2018-04-02 12:38:13 +0530

    getting ticket list added

--- app.js
+++ app.js
@@ -17,3 +17,11 @@ client.users.auth(function (err, res, result) {
   console.log(JSON.stringify(result));
   console.log(JSON.stringify(result.verified, null, 2, true));
 });
+
+client.tickets.list(function (err, statusList, body, responseList, resultList) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(JSON.stringify(body, null, 2, true));//will display all tickets
+});
